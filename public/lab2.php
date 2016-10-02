aab676b359ffd83640644aef02d70edf92e50b4c
Boris Nielsen <borisln@outlook.com>
2016-10-02 18:15:03 +0200

    Lab 2

---
+++ public/lab2.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boris
+ * Date: 28-09-2016
+ * Time: 22:04
+ */
+
+# Exercise 1
+$total = 10 + 14;
+print($total);
+echo "<br>";
+
+# Exercise 2
+$a = 5;
+$b = 2;
+$c = 3;
+$result = $a + $b * $c;
+print($result);
+echo "<br>";
+
+# Exercise 3
+# (°F - 32) / 1.8 = °C
+$f = 95.0;
+$c = ($f - 32) / 1.8;
+print $c;
+
+
+?>
